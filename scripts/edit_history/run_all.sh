689d70c34a562a43aa08af19d56e8c872b6af88f
Alejandro González Hevia <alejandrgh11@gmail.com>
2022-02-06 20:43:41 +0100

    Tmp

--- scripts/edit_history/run_all.sh
+++ scripts/edit_history/run_all.sh
@@ -10,7 +10,7 @@ echo "Dumps downloaded"
 
 cd wd_diff_calculator
 cargo build --release
-#cargo run --release -- --input-dir ../../../data/edit_history/raw_dumps --output-dir ../../../data/edit_history/diffs --bulk-size 100 --entities-file ../../../data/edit_history/entities_ids.txt
+cargo run --release -- --input-dir ../../../data/edit_history/raw_dumps --output-dir ../../../data/edit_history/diffs --bulk-size 100 --entities-file ../../../data/edit_history/entities_ids.txt > progress.txt
 
 cd ../diff_indexer
 cargo build --release

df885e3e5ae029884305a01d25c871d5acc3d509
alejgh <alejandrgh11@gmail.com>
2022-06-03 19:09:22 +0000

    Update run_all.sh

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,13 +5,13 @@
 cd scripts/edit_history
 
 echo "Downloading dumps..."
-#sh download_dumps.sh
+sh download_dumps.sh
 echo "Dumps downloaded"
 
 cd wd_diff_calculator
 cargo build --release
-#cargo run --release -- --input-dir ../data/edit_history/raw_dumps --output-dir ../data/edit_history/diffs --bulk-size 2000 --entities-file ../data/edit_history/entities_ids.txt
+cargo run --release -- --input-dir ../data/edit_history/raw_dumps --output-dir ../data/edit_history/diffs --bulk-size 2000 --entities-file ../data/edit_history/entities_ids.txt
 
 cd ../diff_indexer
 cargo build --release
-#cargo run --release -- --input-dir ../data/edit_history/diffs --entities-classes-file ../notebooks/output/1_data_fetching/entities_classes_ids.csv --bulk-size 2
+cargo run --release -- --input-dir ../data/edit_history/diffs --entities-classes-file ../notebooks/output/1_data_fetching/entities_classes_ids.csv --bulk-size 2
